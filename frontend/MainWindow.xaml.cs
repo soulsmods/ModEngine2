ff3ed74c2fb105145eaf2e2b1573a3c6284aa6c1
Gary Tierney <gary.tierney@fastmail.com>
2021-03-06 11:28:33 +0000

    Add mockup of UI

--- frontend/MainWindow.xaml.cs
+++ frontend/MainWindow.xaml.cs
@@ -12,13 +12,14 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls;
 
 namespace ModEngine2
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
