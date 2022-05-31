b4bfcc8996c443fee0c752a4febcbb1ece637bd0
Gary Tierney <gary.tierney@fastmail.com>
2022-05-31 19:47:26 +0100

    Doc block should be on declaration, not definition

--- src/modengine/scripting/api_patch.cpp
+++ src/modengine/scripting/api_patch.cpp
@@ -3,14 +3,6 @@
 
 namespace modengine::scripting {
 
-/// Creates a simple Lua binding to the Mod Engine patch functionality.
-/// Script authors can create search/replace patches by calling the `Patch.with_fixed_address`
-/// `Patch.with_pattern`.
-///
-/// \code{.lua}
-/// Patch.with_fixed_address(0x14000000, "90909090") ; write NOPs to 0x140000000
-/// Patch.with_pattern(".sl2", ".sl3") ; replace the literal string ".sl2" with ".sl3"
-/// \endcode
 void bind_patch_api(ModEngine* modengine, sol::state_view& lua)
 {
     MemoryScanner scanner;
