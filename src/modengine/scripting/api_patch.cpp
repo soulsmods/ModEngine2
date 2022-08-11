3245babb96f5d480ebd66a3fc130726c6ad03b6f
Gary Tierney <gary.tierney@fastmail.com>
2022-08-11 22:07:44 +0100

    Promote patching API to top-level, remove hexstring reqiurement

--- src/modengine/scripting/api_patch.cpp
+++ src/modengine/scripting/api_patch.cpp
@@ -6,14 +6,17 @@ namespace modengine::scripting {
 void bind_patch_api(ModEngine* modengine, sol::state_view& lua)
 {
     MemoryScanner scanner;
-    auto patch_table = lua.create_named_table("Patch");
 
-    patch_table.set_function("with_fixed_address", [&scanner](uintptr_t addr, std::string replacement) -> bool {
-        return scanner.replace_at(addr, replace_with(util::hex_string(replacement.c_str())));
+    lua.set_function("hexstring", [](std::string value) -> std::string {
+        return util::hex_string(value.c_str());
     });
 
-    patch_table.set_function("with_pattern", [&scanner](std::string pattern, std::string replacement) -> bool {
-        return scanner.replace(pattern, replace_with(util::hex_string(replacement.c_str())));
+    lua.set_function("patch_at_address", [&scanner](uintptr_t addr, std::string replacement) -> bool {
+        return scanner.replace_at(addr, replace_with(replacement.c_str()));
+    });
+
+    lua.set_function("patch_with_pattern", [&scanner](std::string pattern, std::string replacement) -> bool {
+        return scanner.replace(pattern, replace_with(replacement.c_str()));
     });
 }
 }
